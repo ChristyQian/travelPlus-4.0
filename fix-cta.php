b7747fc868ead13a04cd322c4a00584a26584262
ChristyQian <christyQian@christyQian-iMac.local>
2017-05-12 11:22:53 +0800

    Fixed the conflict;

--- fix-cta.php
+++ fix-cta.php
@@ -21,16 +21,16 @@
 					</div>
 				</div>
 				<div class="flex-row-center-between">
-			        <div class="flex-row-center-start"><div class="icon Oval-5"><i class="fa fa-wechat"></i></div>微信咨询</div>
+			        <div id="sticky-wechat" class="flex-row-center-start"><div class="icon Oval-5"><i class="fa fa-wechat"></i></div>微信咨询</div>
 			        	<div class="flex-row-center-end">
 				        	<div class="hotline flex-row-center-end">
-				        		<div class="flex-no-grow-no-shrink-auto icon Oval"><a class="call" href="tel:400-180-7518" onclick="s_objectID=&quot;tel:400-180-7518_2&quot;;return this.s_oc?this.s_oc(e):true"></a></div>
+				        		<div  id="sticky-hotline" class="flex-no-grow-no-shrink-auto icon Oval"><a class="call" href="tel:400-180-7518" onclick="s_objectID=&quot;tel:400-180-7518_2&quot;;return this.s_oc?this.s_oc(e):true"></a></div>
 				        		<div class="flex-no-grow-no-shrink-auto">
 					        		<p>来电咨询</p>
 					        		<span>400-180-7518</span>
 				        		</div>
 				        	</div>
-				        	<a class="orangeBtn" href="#getPersonalizedPrice">
+				        	<a id="sticky-get-price" class="orangeBtn" href="#getPersonalizedPrice">
 				        		获取价格
 				        	</a> 
 			        	</div>
